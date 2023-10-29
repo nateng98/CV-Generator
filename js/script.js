da47394588938073831166a7779de7d0215c3b80
Nước Mắm Code <nateng98@users.noreply.github.com>
2023-10-29 04:46:47 -0500

    Base html and script
    main input boxes and option to add more input boxes

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// jquery from repeater: use for repeatedly delete or add more input from users
+// For those who do not understand this, I provide some explaination within lines of code
+
+$(document).ready(function () {   // make sure that the page is ready (fully loaded) before executing any scripts
+  $('.repeater').repeater({   // select all class repeater and apply repeater fuction to them
+    initEmpty: false,
+    defaultValues: {        // default input is empty string
+      'text-input': ''
+    },
+    show: function () {        // when this is called ([+] button), the input element will drop down
+      $(this).slideDown();// with slidedown animation
+    },
+    hide: function (deleteElement) {  // when this is called ([-] button), the input element will disappear
+      $(this).slideUp(deleteElement); // with slideup animation
+      setTimeout(() => {  // there will be delay of 500ms and call function generateCV()
+        generateCV();
+      }, 500);
+    },
+    isFirstItemUndeletable: true    // repeater plugin that says the first input element cannot be deleted
+  })
+})
